f5a471de781349232292561155f95b9756b7d71b
godfreydekew <dekewgodfrey@gmail.com>
2023-12-02 21:23:33 -0800

    adding s to arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    print("{:d} arguments:".format(len(sys.argv) -1))
+    args = "arguments"
+    if len(sys.argv) == 2:
+        args = "argument"
+
+    print("{:d} {:s}:".format(len(sys.argv) -1, args))
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
-
