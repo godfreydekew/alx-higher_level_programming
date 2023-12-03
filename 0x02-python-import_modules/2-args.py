c352e6331d0766befa1c6df75a6f2939ce2f0006
godfreydekew <dekewgodfrey@gmail.com>
2023-12-02 21:37:14 -0800

    adding . to 0 arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    args = "arguments"
+    args = "arguments:"
     if len(sys.argv) == 2:
-        args = "argument"
+        args = "argument:"
+    if len(sys.argv) == 1:
+        args = "arguments."
 
-    print("{:d} {:s}:".format(len(sys.argv) - 1, args))
+    print("{:d} {:s}".format(len(sys.argv) - 1, args))
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
