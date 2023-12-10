98f9e83c712212caeda40c9c2e9b397e6db86c0b
godfreydekew <dekewgodfrey@gmail.com>
2023-12-10 01:05:28 -0800

    adding missing usr/bin/python3

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    """A function that returns a key with the biggest integer value"""
     if a_dictionary is not None:
         my_max = max(a_dictionary.values())
         for k, v in a_dictionary.items():
             if v == my_max:
                 return k
     else:
-        return None
+        return 'None'
