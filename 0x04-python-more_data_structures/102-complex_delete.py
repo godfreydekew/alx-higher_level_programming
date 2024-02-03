2ff0b40e888463d7ef9bb8cc5c29a437de1ba784
godfreydekew <dekewgodfrey@gmail.com>
2024-02-03 10:15:45 -0800

    Add second return

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-
 def complex_delete(a_dictionary, value):
+    if value not in a_dictionary.values():
+        return a_dictionary
     return {key: val for key, val in a_dictionary.items() if val != value}
