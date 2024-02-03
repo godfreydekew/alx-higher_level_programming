fc7c484856ab7d5a6f03f706cdeac6f02eafd227
godfreydekew <dekewgodfrey@gmail.com>
2024-02-03 09:29:50 -0800

    Task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    score = list(map(lambda x: x[0] * x[1], my_list))
+    weight = list(map(lambda x: x[1], my_list))
+    return sum(score) / sum(weight)
