e0b94a45c05c46650257f02cc4c547f5f2d8c370
godfreydekew <dekewgodfrey@gmail.com>
2024-02-03 10:22:54 -0800

    Use del

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value not in a_dictionary.values():
-        return a_dictionary
-    return {key: val for key, val in a_dictionary.items() if val != value}
+    if value in a_dictionary.values():
+        for k, val in a_dictionary.items():
+            if val == value:
+                del a_dictionary[k]
+                break
+    return a_dictionary
