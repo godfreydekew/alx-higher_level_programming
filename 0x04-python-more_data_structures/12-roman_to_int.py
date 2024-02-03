ba390ca81840880d1fb917bb25f8a0e101e46e85
godfreydekew <dekewgodfrey@gmail.com>
2024-02-03 09:11:06 -0800

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    r_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    total = 0
+    if not isinstance(roman_string, str) or not roman_string:
+        return None
+    prev_value = r_dict[roman_string[-1]]
+    for c in reversed(roman_string):
+        current_value = r_dict[c]
+        if current_value >= prev_value:
+            total += current_value
+        else:
+            total -= current_value
+        prev_value = current_value
+    return int(total)
