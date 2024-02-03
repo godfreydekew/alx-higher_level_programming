fb3e6805a2619a85a788cb77b1afbc3a54808ec7
godfreydekew <dekewgodfrey@gmail.com>
2024-02-03 05:58:38 -0800

    Add del function

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    
     if idx in range(len(my_list)):
-        return [i for i in my_list if my_list.index(i) != idx]
+        del my_list[idx]
     return my_list
