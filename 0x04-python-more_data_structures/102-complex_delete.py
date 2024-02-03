50a94e981822e9d6904485355fd2d83e53763446
godfreydekew <dekewgodfrey@gmail.com>
2024-02-03 10:24:42 -0800

    Use while

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value in a_dictionary.values():
+    while value in a_dictionary.values():
         for k, val in a_dictionary.items():
             if val == value:
                 del a_dictionary[k]
