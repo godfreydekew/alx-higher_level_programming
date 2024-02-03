744afd39021e3bb9a8ffb7e1454d2a0184f96599
godfreydekew <dekewgodfrey@gmail.com>
2024-02-03 09:12:23 -0800

    Add return as 0

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     r_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
     if not isinstance(roman_string, str) or not roman_string:
-        return None
+        return 0
     prev_value = r_dict[roman_string[-1]]
     for c in reversed(roman_string):
         current_value = r_dict[c]
