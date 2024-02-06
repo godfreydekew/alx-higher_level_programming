de53913aa43c6b7958c3e50fffc306d867ed67ea
godfreydekew <dekewgodfrey@gmail.com>
2024-02-06 12:34:06 -0800

    Tsk 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines   BaseGeometry """
+
+
+class BaseGeometry:
+    """A class with no attributes and one
+    method area
+    """
+    def area(self):
+        raise Exception(" area() is not implemented")
