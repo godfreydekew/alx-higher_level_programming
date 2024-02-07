3b7abf3890aed50aabec144ae5d77e7bebd77498
godfreydekew <dekewgodfrey@gmail.com>
2024-02-07 02:46:42 -0800

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines read_file function"""
+
+
+def read_file(filename=""):
+    """Reads and prints the content of a text file.
+
+        Args:
+            filename (str): The name of the file to be read.
+
+        Returns:
+            None
+        """
+    with open(filename, encoding="utf-8") as my_file:
+        print(my_file.read())
