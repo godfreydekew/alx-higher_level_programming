2abc44b444986bb3293b6e41f5761659c7a67d01
godfreydekew <dekewgodfrey@gmail.com>
2024-02-07 02:50:53 -0800

    Remove newline at end

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,4 +12,4 @@ def read_file(filename=""):
             None
         """
     with open(filename, encoding="utf-8") as my_file:
-        print(my_file.read())
+        print(my_file.read(), end="")
