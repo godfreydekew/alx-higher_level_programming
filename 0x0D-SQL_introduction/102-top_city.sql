c54e42b4f7bedb681ad4689b7e08d3a885eed261
godfreydekew <dekewgodfrey@gmail.com>
2024-02-13 11:21:59 -0800

    Use OR

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -4,7 +4,7 @@
 -- displays the top 3 of cities 
 SELECT `city` , AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE `month` IN (7, 8)
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC; 
 LIMIT 3;
