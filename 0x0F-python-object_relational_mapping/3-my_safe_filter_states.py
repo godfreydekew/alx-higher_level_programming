2e4311157e7a8cb12f429e0e0f512df8dc2a3a38
godfreydekew <dekewgodfrey@gmail.com>
2024-03-16 13:23:10 -0700

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+     displays all values in the states table of
+     hbtn_0e_0_usa where name matches the argument.
+"""
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC;"
+    cur.execute(query.format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
