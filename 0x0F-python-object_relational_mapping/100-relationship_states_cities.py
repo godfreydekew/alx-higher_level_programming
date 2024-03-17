7a6d930c453dfae7babeed9bc83dd344c70eb0d8
godfreydekew <dekewgodfrey@gmail.com>
2024-03-17 14:06:58 -0700

    Task 15

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Delete all state that contain letter a"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    # Create a session for interacting with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    newCity = City(name="San Francisco")
+    newState = State(name="California")
+    newState.cities.append(newCity)
+    session.add(newState)
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" create a table states using sqlAlchemy"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        Defines State class
+        __tablename__(str): name of the table
+        id(int): Primary key of the table
+        name(str): state name
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+            "City", backref="state", cascade="all, delete-orphan")
