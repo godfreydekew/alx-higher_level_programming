8c06e766c3c85f3c10fbf4fb9ba31958bf51101e
godfreydekew <dekewgodfrey@gmail.com>
2024-04-10 16:48:37 -0700

    Task 8

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Make a request to the server and follow redirects
-curl -L -X PUT -s -d "user_id=98" -H "Referer: You got me!" http://0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
