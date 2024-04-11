af1556946828b5e5902d4cd00dd15cbc7510c5b8
godfreydekew <dekewgodfrey@gmail.com>
2024-04-11 07:18:28 -0700

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    
+    with urllib.request.urlopen(url) as response:
+        """Make a get request using urllib"""
+        body = response.read()
+
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
