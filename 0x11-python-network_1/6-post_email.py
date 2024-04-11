d8fa7eea319646597fccc8acc3e927f9ae02985b
godfreydekew <dekewgodfrey@gmail.com>
2024-04-11 11:57:41 -0700

    Update

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,7 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    data = {'email': sys.argv[2]}
-    response = requests.post(url, json=data)
+    email = sys.argv[2]
+    data = {'email': email}
+    response = requests.post(url, data=data)
     print(response.text)
