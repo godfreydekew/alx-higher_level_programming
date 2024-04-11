e32479cef58960631977b207542257b31152dbef
godfreydekew <dekewgodfrey@gmail.com>
2024-04-11 07:21:42 -0700

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,6 @@ import urllib.request
 
 if __name__ == '__main__':
     url = "https://alx-intranet.hbtn.io/status"
-    
     with urllib.request.urlopen(url) as response:
         """Make a get request using urllib"""
         body = response.read()
