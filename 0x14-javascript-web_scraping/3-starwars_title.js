9caa255f44e2200e640a6dbe8eb9f71857ac6614
godfreydekew <dekewgodfrey@gmail.com>
2024-04-24 14:20:41 -0700

    Task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+
+request(url, (err, response, body) => {
+    console.log(JSON.parse(body).title|| err);
+});
