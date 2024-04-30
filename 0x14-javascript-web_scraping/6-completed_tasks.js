7427574be140e8250974314eaa847747d9f13be0
godfreydekew <dekewgodfrey@gmail.com>
2024-04-30 12:06:56 -0700

    Task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const myDictionary = {};
+let count = 0;
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const myResponse = JSON.parse(body);
+    myResponse.forEach(usr => {
+      if (myDictionary[usr.userId] === undefined) {
+        myResponse.forEach(tasks => {
+          if (tasks.completed && tasks.userId === usr.userId) {
+            count += 1;
+          }
+        });
+        myDictionary[usr.userId] = count;
+        count = 0;
+      }
+    });
+    console.log(myDictionary);
+  }
+});
